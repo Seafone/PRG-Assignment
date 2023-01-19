83f182b68600268369f0fe92eb8c364109ddaac5
natychannn <116020697+natychannn@users.noreply.github.com>
2023-01-19 15:55:31 +0800

    Add Membership.cs

---
+++ PRG ASG/PRG ASG/Membership.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace asg
+{
+    class Membership
+    {
+        public string Status { get; set; }
+
+        public int Points { get; set; }
+
+        public Membership () { }
+
+        public Membership (string status, int points)
+        {
+            Status = status;
+            Points = points;
+        }
+        public void EarnPoints (double EP)
+        {
+            Points = Convert.ToInt32(EP) / 10;
+        }
+
+        public bool ReedemPoints (int RP)
+        {
+            if(RP <= 0)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        public override string ToString()
+        {
+            return "Status: " + Status + "\tPoints: " + Points;
+
+        }
+    }
+}
