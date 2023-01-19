210fef814680dc5f3e1c2b5c1f5a39b81e85570a
natychannn <116020697+natychannn@users.noreply.github.com>
2023-01-19 15:56:05 +0800

    Add Stay.cs

---
+++ PRG ASG/PRG ASG/Stay.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design.Serialization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace asg
+{
+    class Stay
+    {
+        public DateTime CheckinDate { get; set; }
+
+        public DateTime CheckoutDate { get; set; }
+
+        public Stay() { }
+
+        public Stay (DateTime checkinDate, DateTime checkoutDate)
+        {
+            CheckinDate = checkinDate;
+            CheckoutDate = checkoutDate;
+        }
+
+        public double CalculateTotal () 
+        {
+
+        }
+
+        public override string ToString()
+        {
+            return "Check-in Date: " + CheckinDate + "\tCheck-out Date: " + CheckoutDate;
+        }
+    }
+}
