777b856a370d1aca1c608459e47d7f6172de550e
natychannn <116020697+natychannn@users.noreply.github.com>
2023-01-19 15:50:57 +0800

    Add Guest.cs

---
+++ PRG ASG/PRG ASG/Guest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace asg
+{
+    class Guest
+    {
+        private string Name { get; set; }
+
+        private string PassportNum { get; set; }
+
+        public string Name ()
+    }
+}
