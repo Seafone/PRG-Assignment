ae3a530ce9a4cfa4c2a079a166b31d2344214ce7
natychannn <116020697+natychannn@users.noreply.github.com>
2023-01-19 15:53:12 +0800

    Update Guest.cs

--- PRG ASG/PRG ASG/Guest.cs
+++ PRG ASG/PRG ASG/Guest.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,10 +8,29 @@ namespace asg
 {
     class Guest
     {
-        private string Name { get; set; }
+        public string Name { get; set; }
 
-        private string PassportNum { get; set; }
+        public string PassportNum { get; set; }
 
-        public string Name ()
+        public Stay HotelStay { get; set; }
+
+        public Membership Member { get; set; }
+
+        public bool IsCheckedin { get; set; }
+
+        public Guest () { }
+
+        public Guest (string name, string passportNum, Stay hotelStay, Membership member)
+        {
+            Name = name;
+            PassportNum = passportNum;
+            HotelStay = hotelStay;
+            Member = member;
+        }
+
+        public override string ToString()
+        {
+            return "Name: " + Name + "Passport Number: " + PassportNum + "Hotel Stay: " + HotelStay + "Member: " + Member + "Is Checked-in: " + IsCheckedin;
+        }
     }
 }
