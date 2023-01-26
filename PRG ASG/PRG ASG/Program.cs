50efe325b281f2a70734a681d18d3b0b154bf5d9
natychannn <116020697+natychannn@users.noreply.github.com>
2023-01-26 09:33:08 +0800

    Update Program.cs

--- PRG ASG/PRG ASG/Program.cs
+++ PRG ASG/PRG ASG/Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿void DisplayGuest()
+{
+    string[] info = File.ReadAllLines("Guests.csv");
+    
+    for (int i = 0; i <info.Length;i++)
+    {
+        string[] data = info[i].Split(',');
+        Console.WriteLine("{0,-10} {1,-10} {2,-10} {3,-10}",data[0], data[1], data[2], data[3]);
+    }
+         
+}
+
+DisplayGuest();
