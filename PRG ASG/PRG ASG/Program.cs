a43bae25c983d85a387a3cf0b887078642c96df8
natychannn <116020697+natychannn@users.noreply.github.com>
2023-01-26 10:30:49 +0800

    Update Program.cs

--- PRG ASG/PRG ASG/Program.cs
+++ PRG ASG/PRG ASG/Program.cs
@@ -1,14 +1,16 @@
 using PRG_ASG;
+// Name: Natalie Chan
+// Student Number: S10220879H
 void DisplayGuest()
 {
     string[] info = File.ReadAllLines("Guests.csv");
-    
-    for (int i = 0; i <info.Length;i++)
+
+    for (int i = 0; i < info.Length; i++)
     {
         string[] data = info[i].Split(',');
-        Console.WriteLine("{0,-10} {1,-10} {2,-10} {3,-10}",data[0], data[1], data[2], data[3]);
+        Console.WriteLine("{0,-10} {1,-10} {2,-10} {3,-10}", data[0], data[1], data[2], data[3]);
     }
-         
-}
 
-DisplayGuest();
+}
+// Name: Isaac Khoo
+// Student Number: S10244252C
